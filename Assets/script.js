2a1d43f3ce8bbc1c7b06591e29431113372af93e
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-05 18:55:32 -0700

    fixed duplicated html, and implemented time refresh

--- Assets/script.js
+++ Assets/script.js
@@ -6,13 +6,18 @@
 // - [ ] storing event content into local storage
 // - 
 
-//moment in the header:
+//moment in the header, using set interval to update everyone second
 
 var currenT = $("#currentDay");
 currenT.text(moment().format("dddd, MMM Do YYYY, hh[:]mm[:]ss A"));
-console.log(currenT);
-console.log("test");
-console.log(moment().format("dddd, MMM Do YYYY, hh[:]mm[:]ss A"));
+setInterval(function () {
+    currenT.text(moment().format("dddd, MMM Do YYYY, hh[:]mm[:]ss A"))
+}, 1000);
+// var timer = function () {
+//     va
+
+
+
 
 //dynamically generating time block element using jquery
 //we want 9 blocks from 9 am to 5 pm
@@ -20,4 +25,6 @@ console.log(moment().format("dddd, MMM Do YYYY, hh[:]mm[:]ss A"));
 // $(function () {
 //     var hrBlock = 9;
 //     for (i = 1; 1 < 9; i++) {
-//         $(".container").html("<>")
+$(".container").html("<p>test</p>")
+//     }
+// });
