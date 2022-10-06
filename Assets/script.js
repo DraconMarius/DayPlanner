6ac6a5387772ae6c88302a521f53f2ee0831fd92
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-05 23:11:24 -0700

    sucessfully generated the hour blocks:orz

--- Assets/script.js
+++ Assets/script.js
@@ -24,19 +24,60 @@ setInterval(function () {
 //
 //ok we want a row with the ID of the hours, class of row to store the events
 //inside the row will be the hrs will be 3, 8, 3 with bootstrap grids.
-$(function () {
-    var hours = 0
-    for (i = 0; 1 < 9; i++) {
-        var hrs = i + 9;
-        var btn = $("<btn> Sav </btn>");
-        var evText = $("<textarea></textarea>");
+
+// function newBox() {
+
+$(function (hrBlock) {
+    var hrBlock = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+    // var hours = 0
+    for (var i = 0; i < hrBlock.length; i++) {
+        var cont = $("<div class ='row bg-secondary rounded-lg'></div>");
         //making an empty container with the class of row
-        var cont = $("<div></div>");
-        cont.addClass("row");
-        cont.attr("id", hrs);
-        //adding the 
-        if (i = 0) {
-            $(".container").append(cont);
-        }
+        // cont.addClass("row");
+        // cont.attr('id', hrs)
+        var btn = $("<btn class ='col-2 bg-info border rounded-right-lg'> Sav </btn>");
+        var evText = $("<textarea class ='col-8 bg-secondary'></textarea>");
+        var hrLabel = $("<p class ='col-2 bg-white'></p>");
+        hrLabel.text(hrBlock[i])
+        $(cont).append(hrLabel, evText, btn);
+        //im actually not sure why below is required for the box to show...?
+        $(".container").append(cont);
+
+
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+        //adding the container to the page manually cuz im frustrated at the lack of progress
+        //it was because I wrote a "#1" and not an "i". RIP
+        // if (i = 0) {
+        //     $(".container").append(cont);
+        // } if (i = 1) {
+        //     $(".container").append(cont);
+        // } if (i = 2) {
+        //     $(".container").append(cont);
+        // } if (i = 3) {
+        //     $(".container").append(cont);
+        // } if (i = 4) {
+        //     $(".container").append(cont);
+        // } if (i = 5) {
+        //     $(".container").append(cont);
+        // } if (i = 6) {
+        //     $(".container").append(cont);
+        // } if (i = 7) {
+        //     $(".container").append(cont);
+        // } if (i = 8) {
+        //     $(".container").append(cont);
+        // } if (i = 9) {
+        //     $(".container").append(cont);
+        // } else return
